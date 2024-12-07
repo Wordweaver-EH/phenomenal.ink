655308d6c9e892d8abb3127cf8ff1aaffdcbd703
Wordweaver-EH <wordweaver.eh@gmail.com>
2024-12-07 16:51:31 -0500

    dist after deploy2

--- blog/src/content/config.ts
+++ blog/src/content/config.ts
@@ -1,11 +1,14 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema';
 
+// Extend the docs schema with blog schema
+const docs = defineCollection({
+  schema: docsSchema({
+    extend: blogSchema
+  })
+});
+
 export const collections = {
-  docs: defineCollection({
-    schema: docsSchema({
-      extend: (context) => blogSchema(context)
-    })
-  }),
+  docs
 }; 
